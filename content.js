3bb12b665137d4f1cf130c370b2973ea6100c2a1
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2019-12-26 16:31:28 -0800

    Smooth movement with converted coordinates

--- content.js
+++ content.js
@@ -1,38 +1,44 @@
-let board = document.getElementsByTagName('cg-board')[0];
+const board = document.getElementsByTagName('cg-board')[0];
 console.log('loaded');
 
 // Sample coordinates: (637, 552) -> (624, 403)
 
-function action() {
+// coord from bottom left
+function chessToEuclidSpace(x, y) {
+  let rect = board.getBoundingClientRect();
+  let xStep = rect.width / 8;
+  let yStep = rect.height / 8;
+
+  return [
+    rect.x + x * xStep + xStep / 2,
+    rect.y + y * yStep + yStep / 2
+  ];
+}
+
+function animateMove(startX, startY, endX, endY, duration) {
   let start = Date.now();
-  console.log(start); // DEBUG
 
-  let x = 620;
-  let startY = 552;
-  let endY = 403;
-
-  board.dispatchEvent(downEvent(x, startY));
+  board.dispatchEvent(downEvent(startX, startY));
   let id = setInterval(frame, 5);
+
   function frame() {
     let now = Date.now();
-    let t = (now - start) / 500;
+    let t = (now - start) / duration;
     if (t > 1) {
       clearInterval(id);
-      document.dispatchEvent(upEvent(x, endY));
+      document.dispatchEvent(upEvent(endX, endY));
       return;
     }
 
-    let res = coordEaseInOut(t, x, startY, x, endY);
+    let res = coordEaseInOut(t, startX, startY, endX, endY);
     document.dispatchEvent(moveEvent(res[0], res[1]));
   }
+}
 
-  // board.dispatchEvent(leftClick());
-  // setTimeout(() => {
-  //   document.dispatchEvent(moveEvent());
-  //   setTimeout(() => {
-  //     document.dispatchEvent(upEvent());
-  //   }, 200);
-  // }, 200);
+function action() {
+  let start = chessToEuclidSpace(4, 6);
+  let end = chessToEuclidSpace(4, 4);
+  animateMove(start[0], start[1], end[0], end[1], 500);
 }
 
 document.addEventListener('keydown', (ev) => {
@@ -42,9 +48,9 @@ document.addEventListener('keydown', (ev) => {
   }
 });
 
-document.addEventListener('mousemove', (ev) => {
-  console.log(ev);
-});
+// document.addEventListener('mousemove', (ev) => {
+//   console.log(ev);
+// });
 
 // board.addEventListener('mousedown', (ev) => {
 //   console.log(ev);
