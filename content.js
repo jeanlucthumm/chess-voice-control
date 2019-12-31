38db16b6762103a23dbb961725c55f4d4a6a94a0
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2019-12-30 20:32:12 -0500

    Inject indicator modal

--- content.js
+++ content.js
@@ -2,16 +2,46 @@ console.log('loaded');
 main();
 
 async function main() {
-    let state_board = new Board();
-    state_board.initialize(board);
-
-    let move = 'e4';
-    let [start, end] = state_board.getMoveCoordinates(move);
-    console.log(start, end);
-    start = chessToEuclidSpace(start.x, start.y);
-    end = chessToEuclidSpace(end.x, end.y);
+    // let state_board = new Board();
+    // state_board.initialize(board);
+    //
+    // let move = 'e4';
+    // let [start, end] = state_board.getMoveCoordinates(move);
+    // console.log(start, end);
+    // start = chessToEuclidSpace(start.x, start.y);
+    // end = chessToEuclidSpace(end.x, end.y);
     // await animateMove(start.x, start.y, end.x, end.y, 900);
-    await simpleMove(start, end);
+    // await simpleMove(start, end);
+
+    injectModal();
+}
+
+function injectModal() {
+    // CSS
+    let style = document.createElement('link');
+    style.id = 'chess-voice-control-stylesheet';
+    style.rel = 'stylesheet';
+    style.type = 'text/css';
+    style.href = chrome.extension.getURL('indicator.css');
+    (document.head || document.documentElement).appendChild(style);
+
+    // HTML
+    let div = document.createElement('div');
+    div.id = 'chess-voice-control-content-wrapper';
+    document.body.appendChild(div);
+
+    let request = new XMLHttpRequest();
+    request.open('GET', chrome.extension.getURL('indicator.html'));
+    request.send();
+    request.onload = () => {
+        if (request.status >= 200 && request.status < 400) {
+            div.innerHTML = request.responseText;
+        }
+        let indicator = document.getElementById('chess-voice-control-indicator');
+        if (indicator == null) console.error("Could not find injected modal");
+        indicator.src = chrome.extension.getURL('images/green-circle.svg');
+    };
+    request.send();
 }
 
 function action() {
